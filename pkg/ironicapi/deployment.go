fff21872d58bd349abfa378e9960ddc775570ef1
Steve Baker <sbaker@redhat.com>
2022-09-07 13:15:59 +1200

    Run kolla_httpd_setup on ironic-api start
    This generates the localhost.crt required by httpd ssl.conf

--- pkg/ironicapi/deployment.go
+++ pkg/ironicapi/deployment.go
@@ -30,7 +30,7 @@ import (
 
 const (
 	// ServiceCommand -
-	ServiceCommand = "/usr/local/bin/kolla_set_configs && /usr/local/bin/kolla_start"
+	ServiceCommand = "/usr/local/bin/kolla_httpd_setup && /usr/local/bin/kolla_set_configs && /usr/local/bin/kolla_start"
 )
 
 // Deployment func
