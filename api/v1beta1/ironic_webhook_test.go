f4cbfb81c538289a295bed7cb4bf871f392b8ccd
Abhishek Bongale <abhishekbongale@outlook.com>
2025-08-26 17:17:03 +0100

    [OSPRH-19177] Add unit test for Webhook(DHCPRange)
    This commit focus is to add unit test for the ironic_webhook validation
     DHCPRange function.
    
    Note: I will be adding unit test for other function as well. This is
    starting point for faster validation.
    
    Signed-off-by: Abhishek Bongale <abhishekbongale@outlook.com>

---
+++ api/v1beta1/ironic_webhook_test.go
@@ -0,0 +1,182 @@
+/*
+Copyright 2023 Red Hat Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+
+package v1beta1
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+
+	"k8s.io/apimachinery/pkg/util/validation/field"
+)
+
+func TestValidateDHCPRange(t *testing.T) {
+	testCases := []struct {
+		name         string
+		dhcpRange    DHCPRange
+		path         *field.Path
+		expectedErrs field.ErrorList
+	}{
+		{
+			name: "Valid DHCP range",
+			dhcpRange: DHCPRange{
+				Cidr:    "192.168.1.0/24",
+				Start:   "192.168.1.10",
+				End:     "192.168.1.20",
+				Gateway: "192.168.1.1",
+			},
+			path:         field.NewPath("spec").Child("ironicInspector").Child("dhcpRanges").Index(0),
+			expectedErrs: nil,
+		},
+		{
+			name: "Invalid CIDR",
+			dhcpRange: 	DHCPRange{
+						Cidr:  "not-a-cidr",
+						Start: "192.168.1.10",
+						End:   "192.168.1.20",
+			},
+			path: 		field.NewPath("spec").Child("ironicInspector").Child("dhcpRanges").Index(0),
+			expectedErrs: 	field.ErrorList{
+				field.Invalid(
+					field.NewPath("spec").Child("ironicInspector").Child("dhcpRanges").Index(0).Child("cidr"),
+					"not-a-cidr",
+					errInvalidCidr,
+				),
+			},
+		},
+		{
+			name: "Invalid Start IP Address",
+			dhcpRange: 	DHCPRange{
+						Cidr: "192.168.1.0/24",
+						Start: "not-an-ipaddr",
+						End: "192.168.1.20",
+			},
+			path: 		field.NewPath("spec").Child("ironicInspector").Child("dhcpRanges").Index(0),
+			expectedErrs:	field.ErrorList{
+				field.Invalid(
+					field.NewPath("spec").Child("ironicInspector").Child("dhcpRanges").Index(0).Child("start"),
+					"not-an-ipaddr",
+					errNotIPAddr,
+				),
+			},
+		},
+		{
+			name: "Invalid End IP Address",
+			dhcpRange: 	DHCPRange{
+						Cidr: "192.168.1.0/24",
+						Start: "192.168.1.10",
+						End: "not-an-ipaddr",
+			},
+			path: 		field.NewPath("spec").Child("ironicInspector").Child("dhcpRanges").Index(0),
+			expectedErrs:	field.ErrorList{
+				field.Invalid(
+					field.NewPath("spec").Child("ironicInspector").Child("dhcpRanges").Index(0).Child("end"),
+					"not-an-ipaddr",
+					errNotIPAddr,
+				),
+			},
+		},
+		{
+			name: "Invalid Gateway Address",
+			dhcpRange: 	DHCPRange{
+						Cidr: "192.168.1.0/24",
+						Start: "192.168.1.10",
+						End: "192.168.1.20",
+						Gateway: "not-an-ipaddr",
+			},
+			path: 		field.NewPath("spec").Child("ironicInspector").Child("dhcpRanges").Index(0),
+			expectedErrs:	field.ErrorList{
+				field.Invalid(
+					field.NewPath("spec").Child("ironicInspector").Child("dhcpRanges").Index(0).Child("gateway"),
+					"not-an-ipaddr",
+					errNotIPAddr,
+				),
+			},
+		},
+		{
+			name: "Mixed Address Family (start)",
+			dhcpRange: 	DHCPRange{
+						Cidr: "192.168.1.0/24",
+						Start: "2001:db8::1",
+						End: "192.168.1.20",
+						Gateway: "192.168.1.1",
+			},
+			path: 		field.NewPath("spec").Child("ironicInspector").Child("dhcpRanges").Index(0),
+			expectedErrs:	field.ErrorList{
+				field.Invalid(
+					field.NewPath("spec").Child("ironicInspector").Child("dhcpRanges").Index(0),
+					DHCPRange{
+						Cidr: "192.168.1.0/24",
+						Start: "2001:db8::1",
+						End: "192.168.1.20",
+						Gateway: "192.168.1.1",
+					},
+					errMixedAddressFamily,
+				),
+				field.Invalid(
+					field.NewPath("spec").Child("ironicInspector").Child("dhcpRanges").Index(0).Child("start"),
+					"2001:db8::1",
+					fmt.Sprintf(errNotInCidr, "192.168.1.0/24"),
+				),
+				field.Invalid(
+					field.NewPath("spec").Child("ironicInspector").Child("dhcpRanges").Index(0).Child("start"),
+					"2001:db8::1",
+					fmt.Sprintf(errInvalidRange,"2001:db8::1", "192.168.1.20"),
+				),
+				field.Invalid(
+					field.NewPath("spec").Child("ironicInspector").Child("dhcpRanges").Index(0).Child("end"),
+					"192.168.1.20",
+					fmt.Sprintf(errInvalidRange,"2001:db8::1", "192.168.1.20"),
+				),
+			},
+		},
+		{
+			name: "Invalid IPRange",
+			dhcpRange: 	DHCPRange{
+						Cidr: "192.168.1.0/24",
+						Start: "192.168.1.20",
+						End: "192.168.1.10",
+						Gateway: "192.168.1.1",
+			},
+			path: 		field.NewPath("spec").Child("ironicInspector").Child("dhcpRanges").Index(0),
+			expectedErrs:	field.ErrorList{
+				field.Invalid(
+					field.NewPath("spec").Child("ironicInspector").Child("dhcpRanges").Index(0).Child("start"),
+					"192.168.1.20",
+					fmt.Sprintf(errInvalidRange, "192.168.1.20", "192.168.1.10"),
+				),
+				field.Invalid(
+					field.NewPath("spec").Child("ironicInspector").Child("dhcpRanges").Index(0).Child("end"),
+					"192.168.1.10",
+					fmt.Sprintf(errInvalidRange, "192.168.1.20", "192.168.1.10"),
+				),
+			},
+		},
+	}
+
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			errs := validateDHCPRange(tc.dhcpRange, tc.path)
+
+			if !reflect.DeepEqual(errs, tc.expectedErrs) {
+				t.Errorf("validateDHCPRange() failed:\n    expected: %v\n    got:      %v", tc.expectedErrs, errs)
+			}
+		})
+	}
+}
